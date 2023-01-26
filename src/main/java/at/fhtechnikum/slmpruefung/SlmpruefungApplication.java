72181b012b1d818b49b72a4e67828ea15915558a
ramsa <ic18b066@technikum-wien.at>
2023-01-26 18:07:27 +0100

    Initial commit

---
+++ src/main/java/at/fhtechnikum/slmpruefung/SlmpruefungApplication.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.slmpruefung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmpruefungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmpruefungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtechnikum/slmpruefung/SlmpruefungApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.slmpruefung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmpruefungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
