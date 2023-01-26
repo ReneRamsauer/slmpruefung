92923c9e8fd348b3405f8fd6fe9965d1885fb08b
ramsa <ic18b066@technikum-wien.at>
2023-01-26 19:00:13 +0100

    #1 add calcultatio

--- src/main/java/at/fhtechnikum/slmpruefung/Controller/AreaController.java
+++ src/main/java/at/fhtechnikum/slmpruefung/Controller/AreaController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class AreaController {
     @GetMapping("/api/{shape}/value={size}")
