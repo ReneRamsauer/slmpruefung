368f8dda3bca454f777bf3bfa3de8444625b629f
ramsa <ic18b066@technikum-wien.at>
2023-01-26 19:16:08 +0100

    #3 add calcultatio

--- src/test/java/at/fhtechnikum/slmpruefung/SlmpruefungApplicationTests.java
+++ src/test/java/at/fhtechnikum/slmpruefung/SlmpruefungApplicationTests.java
@@ -1,13 +1,33 @@
 package at.fhtechnikum.slmpruefung;
 
+import at.fhtechnikum.slmpruefung.Controller.AreaController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.awt.geom.Area;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SlmpruefungApplicationTests {
 
 	@Test
-	void contextLoads() {
+	 void testCalculationSquare() {
+		/* GiVEN */
+		AreaController controller = new AreaController();
+		/* When */
+		double result = controller.calcArea("square",10.0);
+		/* Then */
+		assertEquals(result,100.0);
+	}
+	@Test
+	public void testCalculationCircle() {
+		/* GiVEN */
+		AreaController controller = new AreaController();
+		/* When */
+		double result = controller.calcArea("circle",10.0);
+		/* Then */
+		assertEquals(result,314.16);
 	}
 
 }
